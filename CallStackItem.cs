4affaaf51fd3e0333944fc67c344f431c59051e9
Дlastaiя <alastair.brown@blackbay.com>
2016-07-18 09:20:48 +1200

    Added CallStackItem

---
+++ CallStackItem.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+
+namespace ForthCompiler
+{
+    public class CallStackItem : UiItem
+    {
+        public Structure Item { get; set; }
+
+        public string Name => Item.Name.Split('.').Skip(1).First();
+        public string AddressFormatted => Parent.Formatter(Item.Value);
+
+        public void Refresh()
+        {
+            OnPropertyChanged(nameof(Name));
+            OnPropertyChanged(nameof(AddressFormatted));
+        }
+    }
+}
